469aa43956287a675a862fff92a9733d74fc8705
notime4dream <125747668+notime4dream@users.noreply.github.com>
2023-09-03 00:22:23 +0300

    Update valid_data.py

--- tests/valid_data.py
+++ tests/valid_data.py
@@ -1,5 +1,5 @@
-correct_logins = ['oamorenina@gmail.com', '79216560536']
-correct_pass = 'Shokoladka812'
+correct_logins = ['oamoona@gmail.com', '79213557965']
+correct_pass = 'Shoomka12'
 
 agreement_url = 'https://b2c.passport.rt.ru/sso-static/agreement/agreement.html'
 agreement_title = 'Публичная оферта о заключении Пользовательского соглашения на использование Сервиса «Ростелеком ID»'
